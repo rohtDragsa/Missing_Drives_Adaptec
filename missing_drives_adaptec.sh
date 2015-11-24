d04d0c4fb5fda913308042ade71243589baa4f20
RohtDragsa <pawel.radko@mycit.ie>
2015-11-24 06:33:29 +0000

    Update missing_drives_adaptec.sh

--- missing_drives_adaptec.sh
+++ missing_drives_adaptec.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#Created:Pawel Radko
-#Purpose: Detect missing drives from the arrays. 
-#Modified:2.2
+#Created:radpawel
+#Modified:2.3
 #color
 RED='\033[0;31m'
 NC='\033[0m' # No Color
@@ -10,15 +9,28 @@ lsPSI="/sbin/lspci"
 #get nbr of ctrls
 nbrAdap=$($lsPSI | grep -i 'Adaptec' | wc -l)
 #get DB type
-dbType=`grep -i server_type_id /etc/hardware.stanza | cut -d = -f 2 | sed 's/ *//g'`
+dbType=`grep -i server_type_id /etc/hardware.amazon.stanza | cut -d = -f 2 | sed 's/ *//g'`
 new_array=()
 
-if [[ -n `$lsPSI | grep "Adaptec Series 6"` ]]; then
+if [[ `ps aux | grep -v grep | grep pmon` ]]; then
+  #statements
+    printf '\e[33m%-79s\e[m\n' "##############################################################################";
+    printf '\e[33m%-79s\e[m\n' "#                         CAUTION  DB instance running                        #";
+    printf '\e[33m%-79s\e[m\n' "##############################################################################";
 
+    echo -n "Continue? (y/n) ";
+    read -e answer </dev/tty;
+    if [[ ! $answer =~ ^[Yy]$ ]]; then
+  	   exit 0;
+    fi
+ fi
+
+
+ if [[ -n `$lsPSI | grep "Adaptec Series 6"` ||  -n `$lsPSI | grep -i "Adaptec "` ]]; then
    case $dbType in
-       LARGE*) expectedhdd=112;;
-       MID*)   expectedhdd=64;;
-       SMALL*) expectedhdd=38;;
+       DBLARGE12) expectedhdd=116;;
+       DBMID12)   expectedhdd=68;;
+       DBSMALL12) expectedhdd=43;;
        *) echo "Expected host type is not found";exit 1;;
    esac
 fi
@@ -32,7 +44,7 @@ for i in $(seq $nbrAdap )
        printf "\n%s%d%s%d%s\n" "Controller:" "$i" " reports " "$repNum" " drives"
        total=$((total+$repNum))
        arrconfHdd=($($arrConf $i PD | egrep -A5 '(Device is a Hard drive)' | grep -i "Reported Channel,Device(T:L)" | cut -d : -f3 | cut -d \( -f2 | sort -n))
-       stanzaHdd=($(grep -i hardware_storage_physical_$x /etc/hardware.stanza | cut -d : -f 3 | sort -n))
+       stanzaHdd=($(grep -i hardware_storage_physical_$x /etc/hardware.amazon.stanza | cut -d : -f 3 | sort -n))
 
        [ ${#arrconfHdd[*]} != ${#stanzaHdd[*]} ] && { printf "%s" "Missing drive"; }
 
